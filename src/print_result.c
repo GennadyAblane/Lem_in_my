f282c7c271e32c7b2e07f1c030275ea7b33c1890
ablane <size-34@yandex.ru>
2020-10-09 12:22:38 +0300

    fix print step ants in room

--- src/print_result.c
+++ src/print_result.c
@@ -6,7 +6,7 @@
 /*   By: ablane <ablane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 13:27:11 by ablane            #+#    #+#             */
-/*   Updated: 2020/10/07 15:07:08 by ablane           ###   ########.fr       */
+/*   Updated: 2020/10/09 12:19:28 by ablane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -236,28 +236,63 @@ int		ft_step_ants_room(t_bilist *solution, int *ant, int ants)
 	return (ft_chek_solution_room_for_ants(solution, an));
 }
 
+int		ft_search_max_len_corridors(t_bilist *solution)
+{
+	int len;
+	int len_max;
+	t_bilist *tmp;
+
+	len_max = 0;
+	tmp = solution;
+	while (tmp)
+	{
+		len = ft_count_len_corridor((t_bilist*)tmp->content);
+		if (len > len_max)
+			len_max = len;
+		tmp = tmp->next;
+	}
+	return (len_max);
+}
+
+int 	ft_print_room(t_bilist *cor, int i, int n)
+{
+	while (cor && n <= i)
+	{
+		if(cor->content_size)
+		{
+			ft_printf("L%d-%s", cor->content_size, (char*)cor->content);
+			if(cor->next && cor->next->content_size)
+				ft_printf(" ");
+		}
+		n++;
+		cor = cor->next;
+	}
+	return (n);
+}
+
 void	print_current_position_ants(t_bilist *solution)
 {
 	t_bilist *tmp;
 	t_bilist *cor;
+	int i;
+	int n;
+	int max_len;
 
+	i = 0;
+	n = 0;
+	max_len = ft_search_max_len_corridors(solution);
 	tmp = solution;
-	while (tmp)
+	while (tmp && i <= max_len)
 	{
 		cor = (t_bilist*)tmp->content;
-		while (cor)
-		{
-			if(cor->content_size)
-			{
-				ft_printf("L%d-%s", cor->content_size, (char*)cor->content);
-				if(cor->next && cor->next->content_size)
-				{
-					ft_printf(" ");
-				}
-			}
-			cor = cor->next;
-		}
+		n = ft_print_room(cor, i, n);
 		tmp = tmp->next;
+		if (n > i && !tmp)
+		{
+			tmp = solution;
+			i++;
+		}
+		n = 0;
 	}
 }
 
